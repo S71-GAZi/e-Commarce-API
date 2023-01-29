23f24e1a528b4a2bf6298e556b4b265fe004143a
“S71-GAZi <sorifullahpavelgazi@gmail.com>
2023-01-29 18:03:16 +0600

    First Setup2

---
+++ e-Commarce_API/e-Commarce_API/DataContext/ApiContext.cs
@@ -0,0 +1,15 @@
+﻿using e_Commarce_API.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace e_Commarce_API.DataContext
+{
+    public class ApiContext : DbContext 
+    {
+        public ApiContext(DbContextOptions<ApiContext> options) : base(options)
+        {
+
+
+        }
+        public DbSet<User> Users { get; set; }
+    }
+}
