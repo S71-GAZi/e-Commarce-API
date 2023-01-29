574d8c69c4312fa264ea434afb044d0465d356bf
“S71-GAZi <sorifullahpavelgazi@gmail.com>
2023-01-29 17:10:00 +0600

    First Configuration

---
+++ e-Commarce_API/e-Commarce_API/Controllers/TockenController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace e_Commarce_API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TockenController : ControllerBase
+    {
+
+        [HttpGet]
+        [Authorize(AuthenticationSchemes = JwtBearerDefaults.AuthenticationScheme)]
+        public async Task<ActionResult<string>> Get()
+        {
+            var token = await HttpContext.GetTokenAsync("access_token");
+            return token;
+        }
+    }
+}
---
+++ e-Commarce_API/e-Commarce_API/Models/User.cs
@@ -0,0 +1,8 @@
+﻿namespace e_Commarce_API.Models
+{
+    public class User
+    {
+        public string? UserName { get; set; }
+        public string? Password { get; set; }
+    }
+}
